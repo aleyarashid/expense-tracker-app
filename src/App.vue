83deb7337396f92c8909b30e1405562ecd5f4d31
Fazle Rabbi <106666655+aleyarashid@users.noreply.github.com>
2025-02-22 20:23:36 +0600

    Update

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,18 @@ import TransactionList from "./components/TransactionList.vue";
 const transactions = ref([]);
 const filter = ref("ALL");
 
-// Add transaction
+
 const addTransaction = (transaction) => {
   transactions.value.unshift(transaction);
   saveToLocalStorage();
 };
 
-// Store local storage
+
 const saveToLocalStorage = () => {
   localStorage.setItem("transactions", JSON.stringify(transactions.value));
 };
 
-// Transactions list
+
 onMounted(() => {
   const savedTransactions = JSON.parse(localStorage.getItem("transactions"));
   if (savedTransactions) {
@@ -30,7 +30,7 @@ const filteredTransactions = computed(() => {
   let x = transactions.value.filter((t) => t.type == filter.value);
   return x;
 });
-// Delete transaction
+
 const deleteTransaction = (id) => {
   transactions.value = transactions.value.filter((t) => t.id !== id);
   saveToLocalStorage();
@@ -38,26 +38,26 @@ const deleteTransaction = (id) => {
 </script>
 
 <template>
-  <div class="container mt-10 p-4">
+  <div class="container mt-5 p-4">
     <h2 style="text-align: center" class="h1 mb-5">Income & Expense Tracker</h2>
 
     <!-- Add Transaction -->
     <AddTransaction @addTransaction="addTransaction" />
 
-    <!-- Filter -->
-    <div class="mb-3">
+
+    <div class="mb-5">
       <div class="form-label text-light" :v-model="filter">
         <input type="radio" id="all" value="All" v-model="filter" />
-        <label for="all" class="text-dark m-1">All</label>
+        <label for="all" class="text-dark m-2">All</label>
 
         <input type="radio" id="INCOME" value="INCOME" v-model="filter" />
-        <label for="INCOME" class="text-dark m-1">Income</label>
+        <label for="INCOME" class="text-dark m-2">Income</label>
         <input type="radio" id="EXPENSE" value="EXPENSE" v-model="filter" />
-        <label for="EXPENSE" class="text-dark m-1">Expense</label>
+        <label for="EXPENSE" class="text-dark m-2">Expense</label>
       </div>
     </div>
 
-    <!-- Display Transactions -->
+    
     <TransactionList :transactions="filteredTransactions" @deleteTransaction="deleteTransaction" />
   </div>
 </template>
